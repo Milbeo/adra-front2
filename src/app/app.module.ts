7e7c6bf9f9111575c1c63a354e8782a9533f7abe
Milbeo <47335072+Milbeo@users.noreply.github.com>
2020-05-15 21:06:18 +0200

    ajustement design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { LostanimalsComponent } from './lostanimals/lostanimals.component';
+import { LoginmodalComponent } from './login/loginmodal/loginmodal.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'accueil', component: CarouselComponent },
-  { path: '', component: CarouselComponent }
+  { path: '', component: CarouselComponent },
+  { path: 'animalperdu', component: LostanimalsComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CarouselComponent
+    CarouselComponent,
+    LostanimalsComponent,
+    LoginmodalComponent
   ],
   imports: [
     BrowserModule,
